b5bf4f629d17b176d06d8caffcbc100c36e68f2f
d1bevz <d.p.bevz@gmail.com>
2022-04-18 02:12:38 +0300

    Fix parser: rename columns with mapper

--- mining/airflow/dags/utils/okx_parser.py
+++ mining/airflow/dags/utils/okx_parser.py
@@ -3,30 +3,55 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 from typing import List
+from collections import OrderedDict
 
 INSTRUMENT_TYPES = ['SPOT', 'MARGIN', 'SWAP']
-INSTRUMENTS_COLUMNS = dict(
-    instType=str,
-    instId=str,
+INSTRUMENTS_COLUMNS = OrderedDict(
+    inst_type=str,
+    inst_id=str,
     uly=str,
     category=int,
-    baseCcy=str,
-    quoteCcy=str,
-    settleCcy=str,
-    ctVal=float,
-    ctMult=float,
-    ctValCcy=str,
-    optType=str,
+    base_ccy=str,
+    quote_ccy=str,
+    settle_ccy=str,
+    ct_val=float,
+    ct_mult=float,
+    ct_val_ccy=str,
+    opt_type=str,
     stk=float,
-    listTime=datetime,
-    expTime=datetime,
+    list_time=datetime,
+    exp_time=datetime,
     lever=float,
-    tickSz=float,
-    lotSz=float,
-    minSz=float,
-    ctType=str,
+    tick_sz=float,
+    lot_sz=float,
+    min_sz=float,
+    ct_type=str,
     alias=str,
     state=str
+)
+
+INSTRUMENTS_COLUMNS_MAPPER = dict(
+    instType='inst_type',
+    instId='inst_id',
+    uly='uly',
+    category='category',
+    baseCcy='base_ccy',
+    quoteCcy='quote_ccy',
+    settleCcy='settle_ccy',
+    ctVal='ct_val',
+    ctMult='ct_mult',
+    ctValCcy='ct_val_ccy',
+    optType='opt_type',
+    stk='stk',
+    listTime='list_time',
+    expTime='exp_time',
+    lever='lever',
+    tickSz='tick_sz',
+    lotSz='lot_sz',
+    minSz='min_sz',
+    ctType='ct_type',
+    alias='alias',
+    state='state'
 )
 
 class OKXParser:
@@ -64,8 +89,9 @@ class OKXParser:
         )
         result = requests.get(self.url + command, params=params, headers=self.headers)
         result = pd.DataFrame(result.json()['data'])
-        if set(result.columns) != set(INSTRUMENTS_COLUMNS.keys()):
+        if set(result.columns) != set(INSTRUMENTS_COLUMNS_MAPPER.keys()):
             raise ValueError('Please fill underlying asset as uly parameter')
+        result = result.rename(mapper=INSTRUMENTS_COLUMNS_MAPPER, axis='columns')
         return result[INSTRUMENTS_COLUMNS.keys()]
 
     def preprocess(self, df, cols):
