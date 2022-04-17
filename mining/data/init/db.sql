f92918af4ed266c893845439271d9c7d9d122a8c
d1bevz <d.p.bevz@gmail.com>
2022-04-17 23:59:00 +0300

    Added crypto_punk user to database

--- mining/data/init/db.sql
+++ mining/data/init/db.sql
@@ -1,5 +1,9 @@
 create schema crypto_trade;
 
+create user crypto_punk with encrypted password 'decentralized';
+
+grant all privileges on schema crypto_trade to crypto_punk;
+
 create table crypto_trade.candlesticks_raw (
                                                ticker text,
                                                ts bigint,
@@ -10,4 +14,79 @@ create table crypto_trade.candlesticks_raw (
                                                vol float8,
                                                volCcy float8,
                                                updated timestamp default now()
+);
+
+create table crypto_trade.instruments_spot (
+                                               instType text,
+                                               instId text,
+                                               uly text,
+                                               category int8,
+                                               baseCcy text,
+                                               quoteCcy text,
+                                               settleCcy text,
+                                               ctVal float8,
+                                               ctMult float8,
+                                               ctValCcy text,
+                                               optType text,
+                                               stk float8,
+                                               listTime timestamp,
+                                               expTime timestamp,
+                                               lever float8,
+                                               tickSz float8,
+                                               lotSz float8,
+                                               minSz float8,
+                                               ctType text,
+                                               alias text,
+                                               state text,
+                                               updated timestamp default now()
+);
+
+create table crypto_trade.instruments_swap (
+                                                 instType text,
+                                                 instId text,
+                                                 uly text,
+                                                 category int8,
+                                                 baseCcy text,
+                                                 quoteCcy text,
+                                                 settleCcy text,
+                                                 ctVal float8,
+                                                 ctMult float8,
+                                                 ctValCcy text,
+                                                 optType text,
+                                                 stk float8,
+                                                 listTime timestamp,
+                                                 expTime timestamp,
+                                                 lever float8,
+                                                 tickSz float8,
+                                                 lotSz float8,
+                                                 minSz float8,
+                                                 ctType text,
+                                                 alias text,
+                                                 state text,
+                                                 updated timestamp default now()
+);
+
+create table crypto_trade.instruments_margin (
+                                                 instType text,
+                                                 instId text,
+                                                 uly text,
+                                                 category int8,
+                                                 baseCcy text,
+                                                 quoteCcy text,
+                                                 settleCcy text,
+                                                 ctVal float8,
+                                                 ctMult float8,
+                                                 ctValCcy text,
+                                                 optType text,
+                                                 stk float8,
+                                                 listTime timestamp,
+                                                 expTime timestamp,
+                                                 lever float8,
+                                                 tickSz float8,
+                                                 lotSz float8,
+                                                 minSz float8,
+                                                 ctType text,
+                                                 alias text,
+                                                 state text,
+                                                 updated timestamp default now()
 );
