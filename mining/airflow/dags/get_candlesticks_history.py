7eb03f83dde45a86687ebbe778f7d33dbe324f95
d1bevz <d.p.bevz@gmail.com>
2022-04-18 04:05:47 +0300

    Added DAG get_candlesticks_history

--- mining/airflow/dags/get_candlesticks_history.py
+++ mining/airflow/dags/get_candlesticks_history.py
@@ -6,6 +6,7 @@ from airflow.utils import db
 from sqlalchemy import create_engine
 import logging
 import pendulum, datetime, time
+from datetime import timedelta
 from utils.okx_parser import OKXParser, CANDLESTICKS_HISTORY_COLUMNS_DT
 
 log = logging.getLogger(__name__)
@@ -25,19 +26,22 @@ instruments = [
 with DAG(
         dag_id='get_candlesticks_history',
         schedule_interval='0 * * * *',
-        start_date=pendulum.datetime(2022, 1, 1, tz="UTC"),
-        catchup=False,
+        start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+        catchup=True,
         tags=['mining', 'public_data']
 ) as dag:
 
     for instrument in instruments:
-        @task(task_id=f"get_{instrument}")
+        @task(task_id=f"get_{instrument}", retries=3, execution_timeout=timedelta(seconds=10))
         def get_candlesticks_history(instrument_id, ts=None):
-            ts = int(time.mktime(datetime.datetime.strptime(ts, '%Y-%m-%dT%H:%M:%S.%f%z').timetuple()) * 1000)
-            print(instrument_id, ts)
+            ts = int(time.mktime(pendulum.parse(ts).timetuple()) * 1000)
             candlesticks_history = parser.get_candlesticks_history(instrument_id, after=ts, limit=60)
-            candlesticks_history = parser.preprocess(candlesticks_history, CANDLESTICKS_HISTORY_COLUMNS_DT)
-            candlesticks_history.to_sql(name='candlesticks_raw', con=postgres, schema='market_data', if_exists='append', index=False)
-            return 'Success'
+            if len(candlesticks_history)>0:
+                candlesticks_history = parser.preprocess(candlesticks_history, CANDLESTICKS_HISTORY_COLUMNS_DT)
+                candlesticks_history.to_sql(name='candlesticks_history', con=postgres, schema='market_data', if_exists='append', index=False)
+                return 'Success'
+            else:
+                return 'No data'
+
 
         get_candlesticks_history(instrument)
